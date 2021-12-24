fbf292816791af0a67258208b66b4db8298bdd70
Wu Jitao <wujitao@aceinna.cn>
2021-12-24 09:02:03 +0800

    commit rghit

---
+++ INS401_Verification.py
@@ -0,0 +1,43 @@
+from INS401_Ethernet import Ethernet_Dev
+from Test_Cases import Test_Section
+from Test_Cases import Test_Case
+from INS401_Tests import Test_Scripts
+from INS401_Tests import Test_Environment
+#import scripts
+
+def main():
+    uut = Ethernet_Dev()
+    print("\n#######   INS401 ETHERNET Interface Verification V1.0   #######\n")
+    
+    result = uut.find_device(times=1)
+    if result != True:
+        return
+    
+    result = uut.ping_device()
+    if result != True:
+        print('Ethernet ping error.')
+        return
+    
+    model = uut.model_string 
+    serial_number = uut.serial_number
+    version = uut.app_version
+    
+    print("\n# UUT Model: ", model)
+    print("\n# UUT Serial Number: ", serial_number)
+    print("\n# UUT Version: ", version)
+    print("")
+
+    env = Test_Environment(uut)
+    env.setup_tests()
+    print("###########  Executing tests...   ##########################\n")
+    env.run_tests()
+    print("##########  Results   #####################################\n")
+    env.print_results()
+
+    file_name = 'test_results_' + str(serial_number) + '_' + str(version) + '.csv'
+    env.log_results(file_name)
+
+
+if __name__ == "__main__":
+
+    main()
