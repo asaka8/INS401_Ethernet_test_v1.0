2ff6eb250af3017b69fd8d3b3b6e36c69af5b17c
asaka8 <376346470@qq.com>
2022-10-13 16:07:27 +0800

    changed: get params from json file

--- src/Ethernet_test/INS401_Test_Center.py
+++ src/Ethernet_test/INS401_Test_Center.py
@@ -1,22 +1,25 @@
-import os
-
-from tqdm import trange
 from ..test_framwork.Test_Logger import TestLogger
 from ..test_framwork.Test_Cases import Test_Section
-from ..test_framwork.Test_Cases import Test_Case
 from ..test_framwork.Test_Cases import Code
 from ..test_framwork.Test_Cases import Condition_Check
 from ..test_framwork.Test_Cases import Condition_Check_dlc
+from ..test_framwork.Jsonf_Creater import Json_Creat
 from .INS401_Test_Function import Test_Scripts
-
-paramsId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
 
 class Test_Environment:
 
     def __init__(self, device):
         self.scripts = Test_Scripts(device)
         self.test_sections = []
+        json_f = Json_Creat()
+        self.properties = json_f.creat()
+        self.paramIds = []
+        self.values = []
+        for i in self.properties["userId"]:
+            paramsId = i["ID"]
+            value = i["value"]
+            self.paramIds.append(paramsId)
+            self.values.append(value)
 
     # Add test scetions & test scripts here
     def setup_tests(self):
@@ -40,25 +43,25 @@ class Test_Environment:
 
         section3 = Test_Section("ID setting verification without restart")
         self.test_sections.append(section3)
-        section3.add_test_case(Condition_Check("'gnss lever arm x' setting verify",  self.scripts.set_parameters_verify, paramsId[0], 0.5))
-        section3.add_test_case(Condition_Check("'gnss lever arm y' setting verify",  self.scripts.set_parameters_verify, paramsId[1], 0.5))
-        section3.add_test_case(Condition_Check("'gnss lever arm z' setting verify",  self.scripts.set_parameters_verify, paramsId[2], 0.5))
-        section3.add_test_case(Condition_Check("'vrp lever arm x' setting verify",  self.scripts.set_parameters_verify, paramsId[3], -0.5))
-        section3.add_test_case(Condition_Check("'vrp lever arm y' setting verify",  self.scripts.set_parameters_verify, paramsId[4], -0.5))
-        section3.add_test_case(Condition_Check("'vrp lever arm z' setting verify",  self.scripts.set_parameters_verify, paramsId[5], -0.5))
-        section3.add_test_case(Condition_Check("'user lever arm x' setting verify",  self.scripts.set_parameters_verify, paramsId[6], 0.5))
-        section3.add_test_case(Condition_Check("'user lever arm y' setting verify",  self.scripts.set_parameters_verify, paramsId[7], 0.5))
-        section3.add_test_case(Condition_Check("'user lever arm z' setting verify",  self.scripts.set_parameters_verify, paramsId[8], 0.5))
-        section3.add_test_case(Condition_Check("'rotation rbvx' setting verify",  self.scripts.set_parameters_verify, paramsId[9], 180))
-        section3.add_test_case(Condition_Check("'rotation rbvy' setting verify",  self.scripts.set_parameters_verify, paramsId[10], 180))
-        section3.add_test_case(Condition_Check("'rotation rbvz' setting verify",  self.scripts.set_parameters_verify, paramsId[11], 180))
+        section3.add_test_case(Condition_Check("'gnss lever arm x' setting verify",  self.scripts.set_parameters_verify, self.paramIds[0], self.values[0]))
+        section3.add_test_case(Condition_Check("'gnss lever arm y' setting verify",  self.scripts.set_parameters_verify, self.paramIds[1], self.values[1]))
+        section3.add_test_case(Condition_Check("'gnss lever arm z' setting verify",  self.scripts.set_parameters_verify, self.paramIds[2], self.values[2]))
+        section3.add_test_case(Condition_Check("'vrp lever arm x' setting verify",  self.scripts.set_parameters_verify, self.paramIds[3], self.values[3]))
+        section3.add_test_case(Condition_Check("'vrp lever arm y' setting verify",  self.scripts.set_parameters_verify, self.paramIds[4], self.values[4]))
+        section3.add_test_case(Condition_Check("'vrp lever arm z' setting verify",  self.scripts.set_parameters_verify, self.paramIds[5], self.values[5]))
+        section3.add_test_case(Condition_Check("'user lever arm x' setting verify",  self.scripts.set_parameters_verify, self.paramIds[6], self.values[6]))
+        section3.add_test_case(Condition_Check("'user lever arm y' setting verify",  self.scripts.set_parameters_verify, self.paramIds[7], self.values[7]))
+        section3.add_test_case(Condition_Check("'user lever arm z' setting verify",  self.scripts.set_parameters_verify, self.paramIds[8], self.values[8]))
+        section3.add_test_case(Condition_Check("'rotation rbvx' setting verify",  self.scripts.set_parameters_verify, self.paramIds[9], self.values[9]))
+        section3.add_test_case(Condition_Check("'rotation rbvy' setting verify",  self.scripts.set_parameters_verify, self.paramIds[10], self.values[10]))
+        section3.add_test_case(Condition_Check("'rotation rbvz' setting verify",  self.scripts.set_parameters_verify, self.paramIds[11], self.values[11]))
 
         section4 = Test_Section("ID setting verification with repower")
         self.test_sections.append(section4)
-        section4.add_test_case(Condition_Check_dlc("ID 1-3 setting random params test",  self.scripts.parameters_set_with_reset, paramsId[0]))
-        section4.add_test_case(Condition_Check_dlc("ID 4-6 setting random params test",  self.scripts.parameters_set_with_reset, paramsId[3]))
-        section4.add_test_case(Condition_Check_dlc("ID 7-9 setting random params test",  self.scripts.parameters_set_with_reset, paramsId[6]))
-        section4.add_test_case(Condition_Check_dlc("ID 10-12 setting random params test",  self.scripts.parameters_set_with_reset, paramsId[9]))
+        section4.add_test_case(Condition_Check_dlc("ID 1-3 setting random params test",  self.scripts.parameters_set_with_reset, self.paramIds[0]))
+        section4.add_test_case(Condition_Check_dlc("ID 4-6 setting random params test",  self.scripts.parameters_set_with_reset, self.paramIds[3]))
+        section4.add_test_case(Condition_Check_dlc("ID 7-9 setting random params test",  self.scripts.parameters_set_with_reset, self.paramIds[6]))
+        section4.add_test_case(Condition_Check_dlc("ID 10-12 setting random params test",  self.scripts.parameters_set_with_reset, self.paramIds[9]))
 
         section5 = Test_Section("Longterm Test")
         self.test_sections.append(section5)
@@ -67,30 +70,33 @@ class Test_Environment:
         section5.add_test_case(Code("time jump in INS Solution packets", self.scripts.ins_solution_gps_time_jump_test))
         section5.add_test_case(Code("time jump in Diagnosis Message packets", self.scripts.dm_solution_gps_time_jump_test))
         section5.add_test_case(Code("time jump in raw IMU packets", self.scripts.imu_solution_gps_time_jump_test))
-        section5.add_test_case(Condition_Check("ID 1-3 setting longterm test",  self.scripts.parameters_set_loop, paramsId[0], [0.3, 0]))
-        section5.add_test_case(Condition_Check("ID 4-6 setting longterm test",  self.scripts.parameters_set_loop, paramsId[3], [-0.1, 0]))
-        section5.add_test_case(Condition_Check("ID 7-9 setting longterm test",  self.scripts.parameters_set_loop, paramsId[6], [0.25, 0]))
-        section5.add_test_case(Condition_Check("ID 10-12 setting longterm test",  self.scripts.parameters_set_loop, paramsId[9], [180, 0]))
+        section5.add_test_case(Condition_Check("ID 1-3 setting longterm test",  self.scripts.parameters_set_loop, self.paramIds[0], [0.3, 0]))
+        section5.add_test_case(Condition_Check("ID 4-6 setting longterm test",  self.scripts.parameters_set_loop, self.paramIds[3], [-0.1, 0]))
+        section5.add_test_case(Condition_Check("ID 7-9 setting longterm test",  self.scripts.parameters_set_loop, self.paramIds[6], [0.25, 0]))
+        section5.add_test_case(Condition_Check("ID 10-12 setting longterm test",  self.scripts.parameters_set_loop, self.paramIds[9], [180, 0]))
 
         section6 = Test_Section("Vehicle code function test")
         self.test_sections.append(section6)
-        section6.add_test_case(Condition_Check("VF33 setting test", self.scripts.vehicle_code_setting_test, paramsId[13], [0x56, 0x46, 0x33, 0x33]))
-        section6.add_test_case(Condition_Check("VF34 setting test", self.scripts.vehicle_code_setting_test, paramsId[13], [0x56, 0x46, 0x33, 0x34]))
-        section6.add_test_case(Condition_Check("VF35 setting test", self.scripts.vehicle_code_setting_test, paramsId[13], [0x56, 0x46, 0x33, 0x35]))
-        section6.add_test_case(Condition_Check("VF36 setting test", self.scripts.vehicle_code_setting_test, paramsId[13], [0x56, 0x46, 0x33, 0x36]))
-        section6.add_test_case(Condition_Check("VF33 vehicle code status test", self.scripts.vehicle_code_status_test, paramsId[13], [0x56 ,0x46, 0x33, 0x33]))
-        section6.add_test_case(Condition_Check("VF34 vehicle code status test", self.scripts.vehicle_code_status_test, paramsId[13], [0x56, 0x46, 0x33, 0x34]))
-        section6.add_test_case(Condition_Check("VF35 vehicle code status test", self.scripts.vehicle_code_status_test, paramsId[13], [0x56, 0x46, 0x33, 0x35]))
-        section6.add_test_case(Condition_Check("VF36 vehicle code status test", self.scripts.vehicle_code_status_test, paramsId[13], [0x56, 0x46, 0x33, 0x36]))
-        section6.add_test_case(Condition_Check("VF33 vehicle code setting params test", self.scripts.vehicle_code_params_test, paramsId[13], [0x56, 0x46, 0x33, 0x33]))
-        section6.add_test_case(Condition_Check("VF34 vehicle code setting params test", self.scripts.vehicle_code_params_test, paramsId[13], [0x56, 0x46, 0x33, 0x34]))
-        section6.add_test_case(Condition_Check("VF35 vehicle code setting params test", self.scripts.vehicle_code_params_test, paramsId[13], [0x56, 0x46, 0x33, 0x35]))
-        section6.add_test_case(Condition_Check("VF36 vehicle code setting params test", self.scripts.vehicle_code_params_test, paramsId[13], [0x56, 0x46, 0x33, 0x36]))
-        section6.add_test_case(Condition_Check_dlc("vehicle table version test", self.scripts.vehicle_table_version_test, 2))
+        section6.add_test_case(Condition_Check("VF33 setting test", self.scripts.vehicle_code_setting_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x33]))
+        section6.add_test_case(Condition_Check("VF34 setting test", self.scripts.vehicle_code_setting_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x34]))
+        section6.add_test_case(Condition_Check("VF35 setting test", self.scripts.vehicle_code_setting_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x35]))
+        section6.add_test_case(Condition_Check("VF36 setting test", self.scripts.vehicle_code_setting_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x36]))
+        section6.add_test_case(Condition_Check("VF33 vehicle code status test", self.scripts.vehicle_code_status_test, self.paramIds[13], [0x56 ,0x46, 0x33, 0x33]))
+        section6.add_test_case(Condition_Check("VF34 vehicle code status test", self.scripts.vehicle_code_status_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x34]))
+        section6.add_test_case(Condition_Check("VF35 vehicle code status test", self.scripts.vehicle_code_status_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x35]))
+        section6.add_test_case(Condition_Check("VF36 vehicle code status test", self.scripts.vehicle_code_status_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x36]))
+        section6.add_test_case(Condition_Check("VF33 vehicle code setting params test", self.scripts.vehicle_code_params_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x33]))
+        section6.add_test_case(Condition_Check("VF34 vehicle code setting params test", self.scripts.vehicle_code_params_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x34]))
+        section6.add_test_case(Condition_Check("VF35 vehicle code setting params test", self.scripts.vehicle_code_params_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x35]))
+        section6.add_test_case(Condition_Check("VF36 vehicle code setting params test", self.scripts.vehicle_code_params_test, self.paramIds[13], [0x56, 0x46, 0x33, 0x36]))
+        section6.add_test_case(Condition_Check_dlc("vehicle table version test", self.scripts.vehicle_table_version_test, self.properties["vehicle code"]["vcode version"]))
 
     def setup_tests_(self):
         '''for update
         '''
+        section5 = Test_Section("Longterm Test")
+        self.test_sections.append(section5)
+        section5.add_test_case(Code("Longterm setup Test", self.scripts.long_term_test_setup))
 
     def run_tests(self):
         for section in self.test_sections:
